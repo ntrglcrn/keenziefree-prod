a86b815446c1b75c6c783153a40f5aad00c540e5
ntrglcrn <alexandrkarb@gmail.com>
2025-06-05 22:18:31 +0500

    fix: restore valid page.tsx for (website)

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,9 +1,8 @@
-import HomePage from "./home";
-import { getAllPosts } from "@/lib/sanity/client";
-
-export default async function IndexPage() {
-  const posts = await getAllPosts();
-  return <HomePage posts={posts} />;
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Главная страница</h1>
+      <p>Контент главной страницы сайта</p>
+    </div>
+  );
 }
-
-// export const revalidate = 60;
